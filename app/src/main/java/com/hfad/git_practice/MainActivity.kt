4f3c4794236320904311090d475113a069a2b50c
Sharan M A <sharanma2002@gmail.com>
2023-04-12 06:44:00 +0530

    experimental commit

--- app/src/main/java/com/hfad/git_practice/MainActivity.kt
+++ app/src/main/java/com/hfad/git_practice/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("second commit")
-        third commit
+
+        println("experimental")
     }
 }
